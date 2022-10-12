476d8fe46319ad6370ab5e7e889f27205e37a108
anushabc <66515706+anushabc@users.noreply.github.com>
2022-10-12 15:41:56 +0530

    Update application.ps1

--- Patient-Risk-Analyzer/scripts/application.ps1
+++ Patient-Risk-Analyzer/scripts/application.ps1
@@ -8,3 +8,10 @@ cd C:\LabFiles
 #execute pipeline 3 this run 
 Set-AzSynapsePipeline -WorkspaceName $synapseworkspaceName -Name "Pipeline 3" -DefinitionFile "C:\LabFiles\Pipeline 3.json"
 Invoke-AzSynapsePipeline -WorkspaceName $synapseworkspaceName -PipelineName "Pipeline 3"
+
+
+$appointment= kubectl get service/appointment -o jsonpath="{.status.loadBalancer.ingress[0].ip}" -n patienthub
+$batchinference = kubectl get service/batchinference -o jsonpath="{.status.loadBalancer.ingress[0].ip}" -n patienthub
+$patient= kubectl get service/patient -o jsonpath="{.status.loadBalancer.ingress[0].ip}" -n patienthub
+$realtimeinference= kubectl get service/realtimeinference -o jsonpath="{.status.loadBalancer.ingress[0].ip}" -n patienthub
+$tts= kubectl get service/tts -o jsonpath="{.status.loadBalancer.ingress[0].ip}" -n patienthub
