9af50f537305554a8a5d441a1a75eabbd95b61a2
Shashank D <83011430+SD-14@users.noreply.github.com>
2022-10-17 13:28:42 +0530

    Update logon.ps1

--- AI-Powered-Call-Center-Intelligence-Solution-Accelerator/scripts/logon.ps1
+++ AI-Powered-Call-Center-Intelligence-Solution-Accelerator/scripts/logon.ps1
@@ -1,18 +1,13 @@
-Start-Transcript -Path C:\WindowsAzure\Logs\logon.txt -Append
-
-
+Start-Transcript -Path C:\WindowsAzure\Logs\Logon.txt -Append
  . C:\LabFiles\AzureCreds.ps1
-
 
 $userName = $AzureUserName # READ FROM FILE
 $password = $AzurePassword # READ FROM FILE
 $Sid = $AzureSubscriptionID # READ FROM FILE
 $deployId = $DeploymentID
 
-
 $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
-
 
 Connect-AzAccount -Credential $cred | Out-Null
 
@@ -20,35 +15,27 @@ $RG=Get-AzResourceGroup
 $RGName=$RG.ResourceGroupName
 $RGLoc=$RG.Location
 $str = "callstorage"+$DeploymentID
-
-                                 
+                             
 $keys=Get-AzCognitiveServicesAccountKey -ResourceGroupName $RGName -Name speech$deployId
 $key1=$keys.key1
-
-
 $textkeys=Get-AzCognitiveServicesAccountKey -ResourceGroupName $RGName -Name textanalytics$deployId
 $key=$textKeys.key1
 
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("","C:\LabFiles\deploy-03.json")
-$WebClient.DownloadFile("","C:\LabFiles\deploy-03.parameters.json")
-
+$WebClient.DownloadFile("https://raw.githubusercontent.com/CloudLabsAI-Azure/Solution-Accelerators/main/AI-Powered-Call-Center-Intelligence-Solution-Accelerator/templates/deploy-03.json","C:\LabFiles\deploy-03.json")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/CloudLabsAI-Azure/Solution-Accelerators/main/AI-Powered-Call-Center-Intelligence-Solution-Accelerator/templates/deploy-03.parameters.json","C:\LabFiles\deploy-03.parameters.json")
 
 dotnet new console
 dotnet add package Microsoft.CognitiveServices.Speech --version 1.22.0
 dotnet add package Microsoft.CognitiveServices.Speech
 
-
 Copy-Item -Path C:\Windows\System32\system32.csproj -Destination C:\Users\demouser
 copy-Item -Path C:\Windows\System32\Program.cs -Destination C:\Users\demouser
-
 
 Remove-Item -Path C:\Users\demouser\Program.cs
 
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("","C:\Users\demouser\Program1.cs")
-
-
+$WebClient.DownloadFile("https://raw.githubusercontent.com/CloudLabsAI-Azure/Solution-Accelerators/main/AI-Powered-Call-Center-Intelligence-Solution-Accelerator/scripts/Program1.cs","C:\Users\demouser\Program1.cs")
 
 (Get-Content -Path "C:\Users\demouser\Program1.cs") | ForEach-Object {$_ -Replace "<speechkey>", "$key1"} | Set-Content -Path "C:\Users\demouser\Program1.cs"
 (Get-Content -Path "C:\Users\demouser\Program1.cs") | ForEach-Object {$_ -Replace "<azregion>", "$RGLoc"} | Set-Content -Path "C:\Users\demouser\Program1.cs"
@@ -73,7 +60,7 @@ $userName = $AzureUserName
 $password = $AzurePassword
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SpektraSystems/CloudLabs-Azure/master/azure-synapse-analytics-workshop-400/artifacts/setup/azcopy.exe" -OutFile "C:\labfiles\azcopy.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/CloudLabsAI-Azure/Solution-Accelerators/main/AI-Powered-Call-Center-Intelligence-Solution-Accelerator/artifacts/azcopy.exe" -OutFile "C:\labfiles\azcopy.exe"
 
 $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
@@ -89,16 +76,19 @@ $storageContext = $storage.Context
 $srcUrl = $null
 $rgLocation = (Get-AzResourceGroup -Name $rgName).Location
           
+New-Item -Path 'C:\audio-input' -ItemType Directory
 
-$srcUrl = "https://experienceazure.blob.core.windows.net/audio-input?sp=racwdli&st=2022-05-17T06:47:07Z&se=2022-12-31T14:47:07Z&sv=2020-08-04&sr=c&sig=xZ5hr6s0ZLGYc1nI8eZkyfcKf9E856r5DfFj136nnGs%3D"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/CloudLabsAI-Azure/Solution-Accelerators/main/AI-Powered-Call-Center-Intelligence-Solution-Accelerator/artifacts/azure-custom-speech_sampledata_test_audio.wav","C:\audio-input\azure-custom-speech_sampledata_test_audio.wav")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/CloudLabsAI-Azure/Solution-Accelerators/main/AI-Powered-Call-Center-Intelligence-Solution-Accelerator/artifacts/call-batch-analytics_sampledata_SampleData-SiriAzureGoogleTalk1.wav","C:\audio-input\call-batch-analytics_sampledata_SampleData-SiriAzureGoogleTalk1.wav")
 
-           
+$srcUrl = "C:\audio-input"          
 $destContext = $storage.Context
 $containerName = "audio-input"
 $resources = $null
 
 $startTime = Get-Date
-$endTime = $startTime.AddDays(2)
+$endTime = $startTime.AddDays(20)
 $destSASToken = New-AzStorageContainerSASToken  -Context $destContext -Container "audio-input" -Permission rwd -StartTime $startTime -ExpiryTime $endTime
 $destUrl = $destContext.BlobEndPoint + "audio-input" + $destSASToken
 
@@ -106,3 +96,6 @@ $srcUrl
 $destUrl
 
 C:\LabFiles\azcopy.exe copy $srcUrl $destUrl --recursive
+
+Stop-Transcript
+Restart-Computer -Force
